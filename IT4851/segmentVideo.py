b049134db683df3ddc264178bcbb9b88089c74d7
Tuan Nguyen Van <38428343+nguyenvantuan2391996@users.noreply.github.com>
2018-12-20 20:29:37 +0700

    Add files via upload

--- IT4851/segmentVideo.py
+++ IT4851/segmentVideo.py
@@ -2,14 +2,26 @@ import numpy as np
 import cv2 as cv
 import os
 from moviepy.editor import *
+from PIL import Image
+import matplotlib.pyplot as plt
 
-cap = cv.VideoCapture('example.mp4')
+cap = cv.VideoCapture('d.mp4')
 count = 0
 i = 0
+j = 1
+arr_frame = []
+arr_ratio = []
 ratiopre = 0
 rationext = 0
-start_time = 0
-end_time = 0
+
+# create dir save frame and keyframe
+try:
+    if not os.path.exists('data'):
+        os.makedirs('data')
+    if not os.path.exists('keyframe'):
+        os.makedirs('keyframe')
+except OSError:
+    print ('Error: Creating directory')
 
 while (cap.isOpened()):
 
@@ -18,14 +30,14 @@ while (cap.isOpened()):
 
     if ret == True:
         print('Read %d frame: ' % count, ret)
-        cv.imwrite("frame%d.jpg" % count, frame)  # save frame as JPEG file
+        cv.imwrite("./data/frame%d.jpg" % count, frame)  # save frame as JPEG file
 
         # Calculate ratio
         if count >= 1:
             if count == 1:
 
-                img1 = cv.imread('frame%d.jpg' % (count - 1))
-                img2 = cv.imread('frame%d.jpg' % count)
+                img1 = cv.imread("./data/frame%d.jpg" % (count - 1))
+                img2 = cv.imread("./data/frame%d.jpg" % count)
 
                 gray1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
                 gray2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
@@ -44,47 +56,57 @@ while (cap.isOpened()):
                 kp1 = kp2
                 des1 = des2
 
-                img2 = cv.imread('frame%d.jpg' % count)
+                img2 = cv.imread("./data/frame%d.jpg" % count)
 
                 gray2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
 
                 kp2, des2 = sift.detectAndCompute(gray2, None)
 
-            # Match descriptors.
-            matches = bf.match(des1, des2)
-
             if len(kp1) == 0 or len(kp2) == 0:
                 rationext = 0
             else:
+                # Match descriptors.
+                matches = bf.match(des1, des2)
                 rationext = len(matches) / (len(des1) + len(des2))
 
-            os.remove('frame%d.jpg' % (count - 1))
+            # os.remove('frame%d.jpg' % (count - 1))
 
-        # Cut clip if ratio > threshold = 0.7
+        # Cut clip if ratio > threshold = 0.069
         if rationext != 0:
 
-            print(rationext - ratiopre)
+            print(abs(rationext - ratiopre))
 
-            if (rationext - ratiopre) > 0.26:
-                # end_time = count / 30
-                #
-                # my_clip = VideoFileClip("intro.mp4")
-                #
-                # clip1 = my_clip.subclip(start_time, end_time)
-                #
-                # processed_vid = clip1.write_videofile('video%d.mp4' % i)
-                #
-                # start_time = end_time
+            # add arr_frame and arr_ratio
+            arr_frame.append(count)
+            arr_ratio.append(abs(rationext - ratiopre))
 
-                cv.imwrite("keyFrame%d.jpg" % i, frame)  # save frame as JPEG file
-                i += 1
+            if abs(rationext - ratiopre) > 0.069:
+
+                if j == 0:
+                    # create image red
+                    img = Image.new('RGB', (60, 30), color='red')
+                    img.save('./data/frame%d.png' % (count - 1))
+
+                    cv.imwrite("./keyframe/keyFrame%d.jpg" % i, frame)  # save frame as JPEG file
+                    i += 1
+                    j += 1
+                else:
+                    j -= 1
 
         ratiopre = rationext
         count += 1
     else:
-        os.remove('frame%d.jpg' % pre)
-        os.remove('frame%d.jpg' % cur)
+        # os.remove('frame%d.jpg' % (count-1))
+        # os.remove('frame%d.jpg' % cur)
         break
+
+#add x and y labels
+plt.xlabel('Frame')
+plt.ylabel('Ratio')
+
+#show plot
+plt.plot(arr_frame, arr_ratio)
+plt.show()
 
 # When everything done, release the capture
 cap.release()
